c8d4b0ebbd5a216b09eaa5883179c45a842c212c
zeguilherme1000 <guitar.rock221@gmail.com>
2026-03-12 22:29:02 -0300

    Add project files.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new AppShell());
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+    }
+}
